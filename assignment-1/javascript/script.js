ea387e4eec13b006f2fe44e8332c90716b8fcb5a
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-05-27 10:17:09 -0400

    java begun

---
+++ assignment-1/javascript/script.js
@@ -0,0 +1,8 @@
+//force of habit, ensuring that the DOM is loaded before any JavaScript runs
+document.addEventListener("DOMContentLoaded", (event) => {
+    const surprisesBtn = document.querySelector('#surprises');
+    const resetBtn = document.querySelector('#reset');
+    const playbackBtn = document.querySelector('#playback');
+    const story = document.querySelector('#story');
+
+});
