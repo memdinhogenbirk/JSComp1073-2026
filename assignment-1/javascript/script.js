ee70d72b1533c37484f040d2c1d57abd93437ae3
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-05-27 16:05:59 -0400

    comments

--- assignment-1/javascript/script.js
+++ assignment-1/javascript/script.js
@@ -1,8 +1,8 @@
 const surprisesBtn = document.querySelector('#surprises');//randomize
 const resetBtn = document.querySelector('#reset');//reset story
 const playbackBtn = document.querySelector('#playback');//outputstory
-const storyDivided = document.querySelectorAll('#storyDivided h4');//divided story output
-//buttons for story elements
+const storyDivided = document.querySelectorAll('#storyDivided h4');//these h4s are where the story will show up
+//buttons for story element column
 const col1Btn = document.querySelector('#Col1btn');
 const col2Btn = document.querySelector('#Col2btn');
 const col3Btn = document.querySelector('#Col3btn');
@@ -10,7 +10,7 @@ const col4Btn = document.querySelector('#Col4btn');
 const col5Btn = document.querySelector('#Col5btn');
 const colBtnArray = [col1Btn, col2Btn, col3Btn, col4Btn, col5Btn];
 
-//arrays for story elements
+//arrays for story element columns
 const col1Array = document.querySelectorAll('#Col1 li');
 const col2Array = document.querySelectorAll('#Col2 li');
 const col3Array = document.querySelectorAll('#Col3 li');
@@ -23,23 +23,40 @@ let indices = [0, 0, 0, 0, 0];
 //output array, default blank values.
 let outputArray = ["______", "______", "______", "______", "______"];
 //forEach btn, add click listener, pass btn index in array as column number to buttonClicked function.
+/*there is one button per column, so the index of the button being clicked will tell the buttonClicked function
+which column to use*/
 colBtnArray.forEach((btn, index) => {
     btn.addEventListener('click', () => buttonClicked(index));
 });
 function buttonClicked(columnNumber) {
     //get current array for column number (handled by forEach and passed as argument to this function).
-    const currentArray = colArray[columnNumber];
+    const currentColumn = colArray[columnNumber];
     //run word highlighting function (see below)
     highlightWord(columnNumber);
     //iterate through array until length of column reached (last word/words in list).
-    if (indices[columnNumber] < currentArray.length) {
-        outputArray[columnNumber] = currentArray[indices[columnNumber]].textContent.slice(2);
-        indices[columnNumber]++;
+    if (indices[columnNumber] < currentColumn.length) {
+        outputArray[columnNumber] = currentColumn[indices[columnNumber]].textContent.slice(2);
+        /*you may be wondering, why .slice(2)? Emojis are apparently worth 2 string indices, 
+        so this removes the emoji and space from the text content of the list item, leaving just the word or phrase.
+        
+        Beyond that, 
+        - outputArray is the final result that is displayed and read aloud by the screen reader.
+        - columnNumber is being used to determine which column the user is on, and is used to index the...
+        currentColumn, which is the column the user is clicking through (e.g. The turkey, mom, dad... or goat, monkey, fish...)
+        - indices is an array that holds an incrementor for each of those columns to determine what string ought to be placed into
+        the final output (also uses columnNumber to determine which index in the indices array should be incremented)
+        To summarize: 
+            - currentColumn = a sub array from colArray, selected by columnNumber arg passed in buttonClicked function
+            - indicies array is indexed using the same columnNumber (e.g. if columnNumber is 1, we are increasing [0,this,0,0,0])
+            - then index (e.g. currentColumn[this]) to say which li element we are on, then get it's textContent
+        */
+        indices[columnNumber]++;//and here is where the indices indexes are incremented
     }
     //if index is at end of array, reset to 0 and start over.
     else {
         indices[columnNumber] = 0;
-        outputArray[columnNumber] = currentArray[indices[columnNumber]].textContent.slice(2);
+        
+        outputArray[columnNumber] = currentColumn[indices[columnNumber]].textContent.slice(2);
         indices[columnNumber]++;
     }
 }
@@ -63,23 +80,22 @@ surprisesBtn.addEventListener('click', function() {
 });
 //function to highlight the words in the column that is currently being used in the story.
 function highlightWord(columnNumber) {
-    const currentArray = colArray[columnNumber];
+    const currentColumn = colArray[columnNumber];
     //remove highlight from all words in column, then add highlight to current word based on index.
-    currentArray.forEach(word => word.classList.remove('target'));
-    if(indices[columnNumber] < currentArray.length) {
-        currentArray[indices[columnNumber]].classList.add('target');
+    currentColumn.forEach(word => word.classList.remove('target'));
+    if(indices[columnNumber] < currentColumn.length) {
+        currentColumn[indices[columnNumber]].classList.add('target');
     }
     //if index is at end of array, reset to 0 and highlight first word in column.
     else{
-        currentArray[0].classList.add('target');
+        currentColumn[0].classList.add('target');
     }
     
 }
 playbackBtn.addEventListener('click', playbackStory);
 async function playbackStory() {
     /*wanted the delayed effect that the original toy had when outputting your story. 
-    Tried multiple methods using setTimeout, didn't work. Found await within an async function, worked.
-    thank you stack overflow https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep*/
+    Tried multiple methods, eventually found a one liner from stackoverflow https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep*/
 
     //making sure the story is completed before letting the playback function run
     if (outputArray.includes("______")) {
