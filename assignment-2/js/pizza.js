a970926455f0c44aaf333a1e2561e68def2e13f0
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-07-20 14:32:55 -0400

    student info "dynamically" added

--- assignment-2/js/pizza.js
+++ assignment-2/js/pizza.js
@@ -1,3 +1,7 @@
+//dynamically added student info
+const studentInfo = document.getElementById("studentInfo");
+studentInfo.textContent = "Name: Michael Number: 200340292"
+
 const size = document.querySelectorAll('input[name="size"]');
 const crust = document.querySelectorAll('input[name="crust"]');
 const sauce = document.querySelectorAll('input[name="sauce"]');
