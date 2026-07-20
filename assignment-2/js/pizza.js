042fd7a45eb72222bdb0a41539c5e1e5b65db7bd
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-07-20 14:27:53 -0400

    grabbing elements for js

---
+++ assignment-2/js/pizza.js
@@ -0,0 +1,8 @@
+const size = document.querySelectorAll('input[name="size"]');
+const crust = document.querySelectorAll('input[name="crust"]');
+const sauce = document.querySelectorAll('input[name="sauce"]');
+const toppings = document.querySelectorAll('input[name="toppings"]');
+const create = document.getElementById('create');
+const reset = document.getElementById('reset');
+
+
