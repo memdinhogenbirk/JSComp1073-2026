364d5aa0d618f26efe98f99a1b1c93f1b40040d1
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-07-27 13:51:15 -0400

    small CSS changes, comments, calling it here

--- assignment-2/js/pizza.js
+++ assignment-2/js/pizza.js
@@ -2,24 +2,27 @@
 const studentInfo = document.getElementById("studentInfo");
 studentInfo.textContent = "Name: Michael Number: 200340292"
 
+// get radio data from form submission
 const formSize = document.querySelectorAll('input[name="size"]');
 const formCrust = document.querySelectorAll('input[name="crust"]');
 const formSauce = document.querySelectorAll('input[name="sauce"]');
 const formCheese = document.querySelectorAll('input[name="cheese"]');
+// get toppings data from check boxes
 const formToppings = document.querySelectorAll('input[name="toppings"]');
+//get buttons
 const order = document.getElementById('order');
 const reset = document.getElementById('reset');
 const pizzas = document.getElementById('pizzas');
 const indexingBtns = Array.from(document.getElementById('indexingBtns').children);
-// add click event listeners to all indexing buttons
+// add click event listeners to all indexing buttons immediately
 indexingBtns.forEach(element => {
     element.addEventListener('click', function(){
-        currentPizza(element.id.split("-")[1]);
-        activeBtn(element);
+        currentPizza(element.id.split("-")[1]); // currentPizza function (see bottom) reveals the pizza the user clicks via the indexbtns
+        activeBtn(element); // index button visually changes too, indicating which pizza corresponds to which index
     })
 });
 
-// arrays to store pizza objects, html li elements (pizza outputs), and delete buttons for pizzas
+// arrays to store pizza objects, html li elements (pizza outputs), and delete buttons (contained in the html li elements) for pizzas
 let pizzaObjects = [];
 let pizzaList = [];
 let deleteBtns = [];
@@ -60,7 +63,7 @@ order.addEventListener('click', function(event){
     );
     //preparing new elements to be added to the page
     let pizza = document.createElement("li");
-    pizza.textContent = newPizza.displayPizza();
+    pizza.textContent = newPizza.displayPizza(); // call displayPizza method for pizza description string
 
     let deleteBtn = document.createElement("button");// create a delete button for the new pizza so it can be removed
     deleteBtn.textContent = "Remove Pizza";
@@ -69,12 +72,14 @@ order.addEventListener('click', function(event){
 
     pizzaObjects.push(newPizza);// add the new pizza object to the pizzaObjects array for future reference and manipulation
     pizza.id = `pizza-${pizzaObjects.length}`;// set id of new el based on length of pizzaObject array
-    pizzaList.push(pizza);// add pizza to pizzaList array for id adjustment upon deletion of a pizza
+    pizzaList.push(pizza);// add pizza to pizzaList array for applications that require the element info, not the object
+    // set all pizza li elements to hidden first
     pizzaList.forEach(p => {
         p.setAttribute("class", "hidden");
     });
+    // then default users view to last pizza added
     pizzaList.at(-1).setAttribute("class", "viewing");
-    activeBtn(indexingBtns[(pizzaList.length -1)]);
+    activeBtn(indexingBtns[(pizzaList.length -1)]);// do the same for index buttons
     
     
     deleteBtns.push(deleteBtn);// add delete button to deleteBtns array for id adjustment upon deletion of a pizza
@@ -97,6 +102,7 @@ order.addEventListener('click', function(event){
         deleteBtns.splice(pizzaId - 1, 1);
         pizzaList.splice(pizzaId - 1, 1);
         hideIndexes(pizzaObjects.length);// hide any indexing buttons that are no longer needed
+        // if the pizzaList isnt empty after deletion, run activeBtn and currentPizza functions to set users view to last pizza in list
         if(pizzaList.length > 0) {
             activeBtn(indexingBtns[(pizzaList.length -1)]);
             currentPizza(indexingBtns[(pizzaList.length -1)]);
@@ -112,25 +118,25 @@ order.addEventListener('click', function(event){
 
     // functions for activating indexing buttons based on number of objects
     
-    function viewIndexes(index){
+    function viewIndexes(index){ // function changes visual appearance of indexes based on an index value passed in parameters
         for (let i = 0; i < index; i++) {
             indexingBtns[i].classList.remove("inactive");
             indexingBtns[i].classList.add("active");
-            indexingBtns[i].removeAttribute("disabled");
+            indexingBtns[i].removeAttribute("disabled"); // function also ensures the button is clickable
         }
     };
-    function hideIndexes(index){
+    function hideIndexes(index){ // function visually changes appearance of indexes to indicate they are not clickable
         for (let i = index; i < indexingBtns.length; i++) {
             indexingBtns[i].classList.add("inactive");
             indexingBtns[i].classList.remove("active");
-            if(indexingBtns[i].classList.contains("currentBtn")){
+            if(indexingBtns[i].classList.contains("currentBtn")){ // ensures the currentBtn visual change is also undone
                 indexingBtns[i].classList.remove("currentBtn");
             }
-            indexingBtns[i].setAttribute("disabled", "true");
+            indexingBtns[i].setAttribute("disabled", "true"); // function also disables the index buttons that should not be clickable anymore
         }
     };
 });
-// function for pizza being viewed
+// function for pizza being viewed, CSS will hide or reveal each pizza depending on the class applied by JS
 function currentPizza(id){
     pizzaList.forEach(pizza => {
         if(pizza.id.split("-")[1] === id){
@@ -143,18 +149,10 @@ function currentPizza(id){
         }
     });
 }
+// function for visually changing index buttons to indicate which one is selected or is being selected
 function activeBtn(current){
     indexingBtns.forEach(btn =>{
         btn.classList.remove("currentBtn");
     })
     current.classList.add("currentBtn");
 }
-
-/*
-Write JavaScript to capture the values from each of the form inputs when the user clicks the 'order' button.
-
-Create a class called Pizza using the parameters from the order form. The class must include at least one method that builds and returns a description of the pizza order as a string.
-
-When the form is submitted and validated, instantiate a new Pizza object from your class using the captured form values. Visually output the pizza description to the HTML page by invoking the method on your Pizza object — the displayed text must come from that method, not directly from the form values. 
-
-*/
