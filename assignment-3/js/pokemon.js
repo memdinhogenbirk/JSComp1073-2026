802f3f4904c8067cfb7c80e5f991806b56682472
memdinhogenbirk <michael.emdinhogenbirk@gmail.com>
2026-08-06 14:03:19 -0400

    more index work

--- assignment-3/js/pokemon.js
+++ assignment-3/js/pokemon.js
@@ -4,7 +4,6 @@ const baseUrl = "https://pokeapi.co/api/v2/pokemon?limit=12&offset=0";
 const searchInput = document.getElementById("search");
 const searchBtn = document.getElementById("searchBtn");
 const pokemonList = document.getElementById("pokemonList");
-const pageCount = document.getElementById("pageCount");
 
 searchBtn.addEventListener("click", (event) => {
     event.preventDefault();
@@ -33,6 +32,7 @@ fetch(url)
     });
 }
 function displayPokemonList(pokemonArray) {
+    pokemonList.innerHTML = "";
     pokemonArray.forEach(pokemon => {
         const div = document.createElement("div");
         const p = document.createElement("p");
@@ -46,21 +46,52 @@ function displayPokemonList(pokemonArray) {
         div.appendChild(img);
         div.appendChild(imgCaption);
         
-        pokemonList.prepend(div);
+        pokemonList.appendChild(div);
     });
 }
 function addPageIndexes(count) {
+    const pageIndex = document.createElement("span");
+    const pageIndexFwd = document.createElement("a");
+    const pageIndexBack = document.createElement("a");
+
+    pageIndexFwd.textContent = ">";
+    pageIndexBack.textContent = "<";
+    pageIndex.innerHTML = "";
     let pageCountNum = Math.ceil(count / 12);
-    for (let i = 1; i <= pageCountNum; i++) {
-        const pageNum = document.createElement("a");
-        pageNum.textContent = i;
+    for (let i = 1; i <= 5; i++) {
+        let pageNum = document.createElement("a");
+        pageNum.textContent = i +" | ";
         pageNum.href = "#";
         pageNum.addEventListener("click", (event) => {
             event.preventDefault();
-            const urlToSearch = `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${(i - 1) * 12}`;
+            let urlToSearch = `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${(i - 1) * 12}`;
             console.log(urlToSearch);
             fetchResults(urlToSearch);
         });
-        pageCount.appendChild(pageNum);
+        pageIndex.appendChild(pageNum);
     }
+    let specificPageNum = document.createElement("a");
+    specificPageNum.textContent = "... | ";
+    specificPageNum.href = "#";
+
+
+    let lastPageNum = document.createElement("a");
+    lastPageNum.textContent = pageCountNum;
+    lastPageNum.href = "#";
+    pageIndex.prepend(pageIndexBack);
+    pageIndex.appendChild(specificPageNum);
+    pageIndex.appendChild(lastPageNum);
+    pageIndex.appendChild(pageIndexFwd);
+    pokemonList.appendChild(pageIndex);
+
+
+    specificPageNum.addEventListener("click", (event) => {
+        event.preventDefault();
+        let specificPage = prompt("Enter a page number between 1 and " + pageCountNum);
+        if (specificPage >= 1 && specificPage <= pageCountNum) {
+            let urlToSearch = `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${(specificPage - 1) * 12}`;
+            console.log(urlToSearch);
+            fetchResults(urlToSearch);
+        }
+    });
 }
